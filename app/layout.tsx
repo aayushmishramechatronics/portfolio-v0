dbe2ac05cfa005356fe669c20185947583de93d0
Aayush Mishra <manofganges@gmail.com>
2025-05-27 12:06:26 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'A.M',
+  title: 'Aayush Mishra',
   description: 'Created by A.M',
   generator: 'v0.dev',
 }
